23b1221c39e8e48881b4c55237924f1c9134469d
Iago Barbosa <iago-cb@hotmail.com>
2021-12-03 15:19:07 -0300

    Cria autenticação JWT para a aplicação

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resources([
-    'customers' => CustomerController::class,
-    'categories' => CategoryController::class,
-]);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['apiJWT']], function (){
+
+    Route::prefix('customers')->group(function () {
+        Route::get('/', [CustomerController::class,'index']);
+        Route::post('/', [CustomerController::class,'store']);
+        Route::put('/{id}', [CustomerController::class,'update']);
+        Route::delete('/{id}', [CustomerController::class,'destroy']);
+        Route::get('/{id}', [CustomerController::class,'show']);
+    });
+    
+    Route::prefix('categories')->group(function () {
+        Route::get('/', [CategoryController::class,'index']);
+        Route::post('/', [CategoryController::class,'store']);
+        Route::put('/{id}', [CategoryController::class,'update']);
+        Route::delete('/{id}', [CategoryController::class,'destroy']);
+        Route::get('/{id}', [CategoryController::class,'show']);
+    });
+});
+
+
+
